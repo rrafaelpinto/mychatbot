d71439f8da5ba7a5b3cfcfbc5e89c790d8c84b5d
Rafael Pinto <rafael.pinto@ifrn.edu.br>
2024-07-13 15:45:36 -0300

    mais ajustes

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -4,6 +4,7 @@ from chatbot.views import about, candidates, chat
 
 urlpatterns = [
     path('candidates/', candidates, name='candidates'),
+    path('', about, name='about'),
     path('about/', about, name='about'),
     path('<slug:slug>/', chat, name='chat'),
 ]
