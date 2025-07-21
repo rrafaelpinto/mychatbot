2a283cdb9224ec86fed98a79e3676aefb17ea935
Rafael Pinto <rafael.pinto@ifrn.edu.br>
2025-07-21 19:08:15 -0300

    Refactor chatbot app and update local settings
    - Modified chatbot/apps.py for improved functionality
    - Removed obsolete chatbot/signals.py
    - Updated mychatbot/local_settings.py configuration

--- chatbot/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatbot'
-
-    def ready(self):
-        import chatbot.signals
--- mychatbot/local_settings.py
+++ mychatbot/local_settings.py
@@ -1,4 +1,4 @@
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '161.35.114.168',
